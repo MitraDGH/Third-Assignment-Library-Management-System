8c0049d3790ae25fb37d56b61360bc73c76aff3b
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-08 19:08:57 +0330

    2.Writing the Library class - defining 3 arraylist for storing books,users,librarians - writing setter and getter for each arraylist

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,84 +1,148 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Library {
-    /*
-    * The library should have a list of books.
-    * The library should have a map of books ISBNs which is linked to the amount of book
-    -> (for example: harry potter -> 4 means there are currently 4 harry potter books)
-    * The library should have a list of users and a list of librarians.
-     */
+
+    static ArrayList<Book> booksList = new ArrayList<>();
+    Map<String, Integer> isbnLinkedToAmount = new HashMap<String, Integer>();
+    static ArrayList<User> users = new ArrayList<>();
+    static ArrayList<Librarian> librarians = new ArrayList<>();
+
+    public static ArrayList<Book> getBooksList() {
+        return booksList;
+    }
+
+    public static void setBooksList(ArrayList<Book> booksList) {
+        Library.booksList = booksList;
+    }
+
+    public Map<String, Integer> getIsbnLinkedToAmount() {
+        return isbnLinkedToAmount;
+    }
+
+    public void setIsbnLinkedToAmount(Map<String, Integer> isbnLinkedToAmount) {
+        this.isbnLinkedToAmount = isbnLinkedToAmount;
+    }
+
+    public static ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public static void setUsers(ArrayList<User> users) {
+        Library.users = users;
+    }
+
+    public static ArrayList<Librarian> getLibrarians() {
+        return librarians;
+    }
+
+    public static void setLibrarians(ArrayList<Librarian> librarians) {
+        Library.librarians = librarians;
+    }
 
     //book related functions
+    //.........................................................
+    public static void addBook(Book book1){
+        booksList.add(book1);
+    }
+    public static void removeBook(Book book1){
+        booksList.remove(book1);
+    }
 
-    public void addBook(){
+    public static Book searchBook(Book book){
+        int count =0;
+        while (count<booksList.size()){
+            if(booksList.get(count).getName().equals(book)){
+                return booksList.get(count);
+            }
+        }
+        return null;///////////////??????
+    }
+
+    public static void updateBook(Book book){
         //TODO
     }
 
-    public void removeBook(){
-        //TODO
+    public void doesBookExist(Book book){
+        if(searchBook(book) != null){
+            System.out.println("Your book was successfully found !");
+        }else  System.out.println("Unfortunately this book doesn't exist here!");
     }
 
-    public void searchBook(){
-        //TODO
+    public void increaseBook(Book book , int count){
+        for (int i = 1 ; i< count ; i++){
+            searchBook(book).increasing();
+        }
     }
 
-    public void updateBook(){
-        //TODO
-    }
-
-    public void doesBookExist(){
-        //TODO
-    }
-
-    public void increaseBook(){
-        //TODO
-    }
-
-    public void decreaseBook(){
-        //TODO
+    public void decreaseBook(Book book , int count){
+        for (int i = 1 ; i< count ; i++){
+            searchBook(book).decreasing();
+        }
     }
 
     //user related functions
+    //.........................................................
+    public static void addUser(User user1){
+        users.add(user1);
+    }
 
-    public void addUser(){
+    public static void removeUser(User user1){
+        users.remove(user1);
+    }
+
+    public static User searchUser(User user){
+        int count =0;
+        while (count<users.size()){
+            if(users.get(count).getUsername().equals(user)){
+                return users.get(count);
+            }
+        }
+        return null;///////////////??????
+    }
+
+    public static void updateUser(User user){
         //TODO
     }
 
-    public void removeUser(){
-        //TODO
-    }
-
-    public void searchUser(){
-        //TODO
-    }
-
-    public void updateUser(){
-        //TODO
-    }
-
-    public void doesUserExist(){
-        //TODO
+    public void doesUserExist(User user){
+        if(searchUser(user) != null){
+            System.out.println("The user was successfully found !");
+        }else  System.out.println("Unfortunately this user id doesn't exist here!");
     }
 
     //librarian related functions
+    //.........................................................
 
-    public void addLibrarian(){
+    public static void addLibrarian(Librarian librarian){
+        librarians.add(librarian);
+    }
+
+    public static void removeLibrarian(Librarian librarian){
+        librarians.remove(librarian);
+    }
+
+    public static Librarian searchLibrarian(Librarian librarian){
+        int count =0;
+        while (count<librarians.size()){
+            if(librarians.get(count).getLibrarianUser().equals(librarian)){
+                return librarians.get(count);
+            }
+        }
+        return null;///////////////??????
+    }
+
+    public static void updateLibrarian(Librarian librarian){
         //TODO
     }
 
-    public void removeLibrarian(){
-        //TODO
+    public void doesLibrarianExist(Librarian librarian){
+        if(searchLibrarian(librarian) != null){
+            System.out.println("The librarian was successfully found !");
+        }else  System.out.println("Unfortunately this librarian doesn't exist here!");
     }
-
-    public void searchLibrarian(){
-        //TODO
-    }
-
-    public void updateLibrarian(){
-        //TODO
-    }
-
-    public void doesLibrarianExist(){
-        //TODO
-    }
+    //.........................................................
 
 
 }
