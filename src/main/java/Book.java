ebb609ca6dc149fc901edca3b91ec13ab4f3bd23
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-08 19:07:17 +0330

    1.Writing the Book class - defining its variables - writing setter and getter for each variable

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,52 @@
 public class Book {
     //Book should contain name,author,year of publish and ISBN
+    public String name;
+    public String author;
+    public int yearOfPublish;
+    public String ISBN ;
+    int bookCounter =0 ;
+
+    <Public> Public Book (String name , String author , int yearOfPublish , String ISBN){
+        this.name=name;
+        this.author=author;
+        this.yearOfPublish=yearOfPublish;
+        this.ISBN=ISBN;
+        return null; /////////////////////////??????????????
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getYearOfPublish() {
+        return yearOfPublish;
+    }
+    public void setYearOfPublish(int yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
+    public String getISBN() {
+        return ISBN;
+    }
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+
+    public int getBookCounter() {
+        return bookCounter;
+    }
+
+    public void setBookCounter(int bookCounter) {
+        this.bookCounter = bookCounter;
+    }
+    public void increasing (){bookCounter++; }
+    public void decreasing (){bookCounter--; }
+
 }
