30b78d5fe58d6e955c3b10b6741717f822686f66
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-08 19:11:30 +0330

    3.Writing the Librarian class - defining 2 variables (user / pass) - writing setter and getter for each one of them - completing add,remove,search methods for users,books and librarians

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -1,10 +1,75 @@
+import java.util.ArrayList;
+
 public class Librarian {
-    /*
-    * The librarian should have a username and a password
-    * The librarian should be able to search users, librarians and books
-    * The librarian should be able to add\remove\update user add\remove\update_
-    _ librarian and add\remove\update book
-     */
+    public String librarianUser;
+    public String librarianPass;
+
+    public Librarian(String librarianUser , String librarianPass){
+        this.librarianUser=librarianUser;
+        this.librarianPass=librarianPass;
+    }
+
+    public String getLibrarianUser() {
+        return librarianUser;
+    }
+
+    public void setLibrarianUser(String librarianUser) {
+        this.librarianUser = librarianUser;
+    }
+
+    public String getLibrarianPass() {
+        return librarianPass;
+    }
+
+    public void setLibrarianPass(String librarianPass) {
+        this.librarianPass = librarianPass;
+    }
+
+    //for searching users, librarians and books
+    //.........................................................
+    public User searchuser (User user){
+        return Library.searchUser(user);
+    }
+    public Librarian searchlibrarian (Librarian librarian){
+        return Library.searchLibrarian(librarian);
+    }
+    public Book searchbook (Book book){
+        return Library.searchBook(book);
+    }
+    //.........................................................
+
+    //for add,remove and update user,librarian and book
+    //.........................................................
+    public void adduser (User user){
+        Library.addUser(user);
+    }
+    public static void addLibrarian(Librarian librarian) {
+        Library.addLibrarian(librarian);
+    }
+    public void addbook (Book book){
+        Library.addBook(book);
+    }
+    //......
+    public void removeuser (User user){
+        Library.removeUser(user);
+    }
+    public void removelibrarian (Librarian librarian){
+        Library.removeLibrarian(librarian);
+    }
+    public void removebook (Book book){
+        Library.removeBook(book);
+    }
+    //......
+    public void updateuser (User user){
+        Library.updateUser(user);
+    }
+    public void updatelibrarian (Librarian librarian){
+        Library.updateLibrarian(librarian);
+    }
+    public void updatebook (Book book){
+        Library.updateBook(book);
+    }
+    //......
 
 
 }
