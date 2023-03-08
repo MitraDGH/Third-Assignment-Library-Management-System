a1c107610ba9ba2869f29f8f7116a63c44a925db
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-08 19:12:51 +0330

    4.Writing the User class - defining 2 variables (user / pass) and an arraylist for user's books - writing setter and getter for each one of them - completing rent and return methods

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,50 @@
-public class User {
-    //User should have a list of books
-    //User should have a username and a password
+import java.util.ArrayList;
 
-    public void rentBook(){
-        //TODO
+public class User {
+    ArrayList<Book> userBooks = new ArrayList<>();
+    public String username;
+    public String pass;
+
+    public User(String username , String pass){
+        this.username=username;
+        this.pass=pass;
     }
 
-    public void returnBook(){
-        //TODO
+    public ArrayList<Book> getUserBooks() {
+        return userBooks;
+    }
+
+    public void setUserBooks(ArrayList<Book> userBooks) {
+        this.userBooks = userBooks;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void rentBook(Book book){
+        Book newbook = Library.searchBook(book);
+        userBooks.add(newbook);
+        newbook.decreasing(); //after renting its number is decreasing in the library
+        ///????????
+    }
+
+    public void returnBook(Book book){
+        Book oldbook = Library.searchBook(book);
+        userBooks.remove(oldbook);
+        oldbook.decreasing(); //after returning its number is decreasing in the library
+        ////?????
     }
 }
